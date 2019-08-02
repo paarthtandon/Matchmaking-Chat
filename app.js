f5d0648b3e783f80a553ed7ab27db62031610e31
Paarth Tandon <paarthri@gmail.com>
2019-08-02 02:38:22 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var socket = require('socket.io');
+var fetch = require("node-fetch");
+
+//App setup
+var app = express();
+var server = app.listen(process.env.PORT || 4000, function () {
+    console.log('listening to request on port 4000');
+})
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+// App Code
+var queue = [];
+var rooms = {};
+
+//Methods
+var matchTags = function (set1, set2) {
+    var result = set1.filter(function (n) {
+        return set2.indexOf(n) > -1;
+    });
+    if (result.length > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+        
+var findPartnerForLoneSocket = function (user) {
+    console.log('user', user.tags, user.blocked);
+
+    if (queue.length > 0) {
+        var partner = '';
+
+        for (let i = 0; i < queue.length; i++) {
+            const p = queue[i];
+
+            if (p) {
+                if (!(user.blocked.includes(p.socket.id) || p.blocked.includes(user.socket.id))) {
+                    //user: any, partner: any
+                    if (p.preference == 'any' && user.preference == 'any') {
+                        if (matchTags(user.tags, p.tags)){
+                            var partner = queue[i];
+                            delete queue[i];
+                            break;
+                        }
+                    //partner: any
+                    } else if (p.preference == 'any') {
+                        if (user.preference == p.gender) {
+                            if (matchTags(user.tags, p.tags)){
+                                var partner = queue[i];
+                                delete queue[i];
+                                break;
+                            }
+                        }
+                    //user: any
+                    } else if (user.preference == 'any') {
+                        if (p.preference == user.gender) {
+                            if (matchTags(user.tags, p.tags)){
+                                var partner = queue[i];
+                                delete queue[i];
+                                break;
+                            }
+                        }
+                    //No any
+                    } else {
+                        if (user.preference == p.gender && p.preference == user.gender) {
+                            if (matchTags(user.tags, p.tags)){
+                                var partner = queue[i];
+                                delete queue[i];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (partner != '') {
+            var room = user.socket.id + '#' + partner.socket.id;
+
+            partner.socket.join(room);
+            user.socket.join(room);
+
+            rooms[partner.socket.id] = room;
+            rooms[user.socket.id] = room;
+
+            partner.socket.emit('chat start', {
+                'room': room,
+                'gender': user.gender
+            });
+            user.socket.emit('chat start', {
+                'room': room,
+                'gender': partner.gender
+            });
+        } else {
+            queue.push(user);
+        }
+    } else {
+        queue.push(user);
+    }
+}
+
+//Listeners
+io.on('connection', function (socket) {
+    console.log('made socket connection', socket.id, queue.length);
+
+    socket.on('search', function (data) {
+        console.log('search ', socket.id, data.gender, data.preference, queue.length);
+        user = {
+            socket: socket,
+            gender: data.gender,
+            preference: data.preference,
+            tags: data.tags,
+            blocked: data.blocked
+        };
+        findPartnerForLoneSocket(user);
+    });
+    socket.on('leave room', function () {
+        var room = rooms[socket.id];
+        delete queue[socket.id];
+        socket.broadcast.to(room).emit('chat end');
+        socket.leave();
+        var partnerID = room.split('#');
+        partnerID = partnerID[0] === socket.id ? partnerID[1] : partnerID[0];
+        console.log('leave ', socket.id, queue.length);
+    });
+    socket.on('disconnect', function () {
+        console.log('disconnect', socket.id);
+        if (rooms.length > 0) {
+            var room = rooms[socket.id];
+            socket.broadcast.to(room).emit('chat end');
+            var partnerID = room.split('#');
+            partnerID = partnerID[0] === socket.id ? partnerID[1] : partnerID[0];
+        }
+        delete queue[socket.id];
+        socket.leave();
+    });
+    socket.on('block', function () {
+        var room = rooms[socket.id];
+        var partnerID = room.split('#');
+        partnerID = partnerID[0] === socket.id ? partnerID[1] : partnerID[0];
+        socket.emit('block', { id: partnerID });
+    });
+    socket.on('message', function (data) {
+        var room = rooms[socket.id];
+        socket.to(room).emit('message', {
+            message: data.message
+        });
+    });
+    socket.on('typing', function (data) {
+        io.to(data.room).emit('typing');
+    });
+    socket.on('done typing', function (data) {
+        io.to(data.room).emit('done typing');
+    });
+});
